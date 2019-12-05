b8950bce87111eda0def2ecba116978c4041b34e
Dan Ott <thedanielott@gmail.com>
2019-12-05 18:50:45 -0500

    💍 Solve day five with a completely new opcode computer
    This was a fun one. Many tiny classes. Hooray.

---
+++ day_05.rb
@@ -0,0 +1,377 @@
+require "minitest/autorun"
+
+class OpcodeTest < Minitest::Test
+  EXAMPLES = {
+    [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50] => [3_500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+    [1, 0, 0, 0, 99] => [2, 0, 0, 0, 99],
+    [2, 3, 0, 3, 99] => [2, 3, 0, 6, 99],
+    [2, 4, 4, 5, 99, 0] => [2, 4, 4, 5, 99, 9_801],
+    [1, 1, 1, 4, 99, 5, 6, 0, 99] => [30, 1, 1, 4, 2, 5, 6, 0, 99],
+    [1_101, 100, -1, 4, 0] => [1_101, 100, -1, 4, 99],
+  }
+
+  def test_examples
+    EXAMPLES.each do |input_state, expected_state|
+      computer = Computer.new(state: input_state)
+      computer.run
+      assert_equal expected_state, computer.state
+    end
+  end
+
+  def test_part_one_find_diagnostic_code
+    puzzle_input = File.read("./day_05.input.txt").split(",").map(&:to_i)
+    computer = Computer.new(state: puzzle_input, input: [1]).run
+    assert_equal 4_887_191, computer.diagnostic_code
+  end
+
+  def test_part_two_example
+    state = [
+      3,
+      21,
+      1_008,
+      21,
+      8,
+      20,
+      1_005,
+      20,
+      22,
+      107,
+      8,
+      21,
+      20,
+      1_006,
+      20,
+      31,
+      1_106,
+      0,
+      36,
+      98,
+      0,
+      0,
+      1_002,
+      21,
+      125,
+      20,
+      4,
+      20,
+      1_105,
+      1,
+      46,
+      104,
+      999,
+      1_105,
+      1,
+      46,
+      1_101,
+      1_000,
+      1,
+      20,
+      4,
+      20,
+      1_105,
+      1,
+      46,
+      98,
+      99,
+    ]
+    input_less_than_eight = Computer.new(state: state, input: [7]).run
+    input_exactly_eight = Computer.new(state: state, input: [8]).run
+    input_greater_than_eight = Computer.new(state: state, input: [9]).run
+    assert_equal 999, input_less_than_eight.diagnostic_code
+    assert_equal 1_000, input_exactly_eight.diagnostic_code
+    assert_equal 1_001, input_greater_than_eight.diagnostic_code
+  end
+
+  def test_part_two_find_diagnostic_code
+    puzzle_input = File.read("./day_05.input.txt").split(",").map(&:to_i)
+    computer = Computer.new(state: puzzle_input, input: [5]).run
+    assert_equal 3_419_022, computer.diagnostic_code
+  end
+end
+
+class Computer
+  attr_reader :state
+  attr_reader :input
+  attr_reader :output
+  attr_reader :halted
+  attr_accessor :position
+
+  def initialize(state: [], input: [])
+    @state = state
+    @input = input
+
+    @output = []
+    @position = 0
+    @halted = false
+  end
+
+  def run
+    while instruction = next_instruction
+      execute(instruction)
+    end
+    self
+  end
+
+  def next_instruction
+    return if halted
+    Instruction.generate(state[position, 4])
+  end
+
+  def execute(instruction)
+    instruction.execute(self)
+  end
+
+  def halt
+    @halted = true
+  end
+
+  def gets
+    fail "No more input provided" if input.empty?
+    input.shift
+  end
+
+  def puts(value)
+    output << value
+  end
+
+  def diagnostic_code
+    return unless halted
+    output.last
+  end
+end
+
+class Paramter
+  def self.generate(mode:, value:)
+    case mode
+    when 0
+      PositionParameter.new(value)
+    when 1
+      ImmediateParameter.new(value)
+    else
+      fail "Unrecognized mode: #{mode}"
+    end
+  end
+end
+
+class ImmediateParameter
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def resolve(_state)
+    value
+  end
+end
+
+class PositionParameter
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def resolve(state)
+    state[value]
+  end
+end
+
+class InstructionParser
+  attr_reader :opcode
+  attr_reader :parameters
+
+  def initialize(state)
+    opcode_chars = state.first.to_s.rjust(5, "0").chars
+    @opcode = opcode_chars.last(2).join.to_i
+    parameter_modes = opcode_chars.first(3).map(&:to_i).reverse
+    raw_parameters = state[1, 3]
+    @parameters =
+      parameter_modes.zip(raw_parameters).map do |mode, value|
+        Paramter.generate(mode: mode, value: value)
+      end
+  end
+end
+
+module Instruction
+  module Registry
+    def self.extended(other)
+      classes << other
+    end
+
+    def self.classes
+      @@classes ||= []
+    end
+
+    def opcode(value = :called_as_getter)
+      if value == :called_as_getter
+        @opcode
+      else
+        @opcode = value
+      end
+    end
+
+    def arity(value = :called_as_getter)
+      if value == :called_as_getter
+        @arity
+      else
+        @arity = value
+      end
+    end
+  end
+
+  def self.included(other)
+    other.extend(Registry)
+  end
+
+  def self.generate(state)
+    parser = InstructionParser.new(state)
+    klass = Registry.classes.find { |candidate| candidate.opcode == parser.opcode }
+    klass.new(parameters: parser.parameters.take(klass.arity))
+  end
+
+  attr_reader :parameters
+
+  def initialize(parameters:)
+    @parameters = parameters
+  end
+
+  def output_address
+    parameters.last.value
+  end
+
+  def length
+    parameters.length + 1
+  end
+
+  private
+
+  def resolve_parameters(computer_state)
+    parameters.map { |p| p.resolve(computer_state) }
+  end
+end
+
+class Add
+  include Instruction
+
+  opcode 1
+  arity 3
+
+  def execute(computer)
+    left, right = resolve_parameters(computer.state)
+    computer.state[output_address] = left + right
+    computer.position += length
+    self
+  end
+end
+
+class Multiply
+  include Instruction
+
+  opcode 2
+  arity 3
+
+  def execute(computer)
+    left, right = resolve_parameters(computer.state)
+    computer.state[output_address] = left * right
+    computer.position += length
+    self
+  end
+end
+
+class Input
+  include Instruction
+
+  opcode 3
+  arity 1
+
+  def execute(computer)
+    computer.state[output_address] = computer.gets
+    computer.position += length
+    self
+  end
+end
+
+class Output
+  include Instruction
+
+  opcode 4
+  arity 1
+
+  def execute(computer)
+    value = resolve_parameters(computer.state).first
+    computer.puts value
+    computer.position += length
+    self
+  end
+end
+
+class JumpIfTrue
+  include Instruction
+
+  opcode 5
+  arity 2
+
+  def execute(computer)
+    first, second = resolve_parameters(computer.state)
+    if first.zero?
+      computer.position += length
+    else
+      computer.position = second
+    end
+  end
+end
+
+class JumpIfFalse
+  include Instruction
+
+  opcode 6
+  arity 2
+
+  def execute(computer)
+    first, second = resolve_parameters(computer.state)
+    if first.zero?
+      computer.position = second
+    else
+      computer.position += length
+    end
+  end
+end
+
+class LessThan
+  include Instruction
+
+  opcode 7
+  arity 3
+
+  def execute(computer)
+    first, second = resolve_parameters(computer.state)
+    computer.state[output_address] = first < second ? 1 : 0
+    computer.position += length
+  end
+end
+
+class Equals
+  include Instruction
+
+  opcode 8
+  arity 3
+
+  def execute(computer)
+    first, second = resolve_parameters(computer.state)
+    computer.state[output_address] = first == second ? 1 : 0
+    computer.position += length
+  end
+end
+
+class Halt
+  include Instruction
+
+  opcode 99
+  arity 0
+
+  def execute(computer)
+    computer.halt
+    computer.position += length
+  end
+end
